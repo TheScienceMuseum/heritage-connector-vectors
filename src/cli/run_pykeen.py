e8946e1f88b4bdcdd9e09181e76e8e81f09f65d3
Kalyan Dutia <kalyan.dutia@gmail.com>
2021-06-30 16:18:11 +0100

    run_pykeen saves embeddings matrices and thing-to-id mapping

--- src/cli/run_pykeen.py
+++ src/cli/run_pykeen.py
@@ -2,6 +2,7 @@ import click
 from pykeen.pipeline import pipeline_from_config
 from pykeen.triples import TriplesFactory
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import json
 from utils import get_timestamp
@@ -65,6 +66,33 @@ def main(input_data_path, output_path, config_path, gpu, plot_losses, random_see
     results = pipeline_from_config(config, **pipeline_kwargs)
     results.save_to_directory(output_path)
 
+    # Save embeddings matrices
+    ent_representations = results.model.entity_representations
+    rel_representations = results.model.relation_representations
+
+    with open(os.path.join(output_path, "entity_embeddings.npy"), "wb") as f:
+        np.save(f, ent_representations[0](indices=None).detach().numpy())
+
+    with open(os.path.join(output_path, "relation_embeddings.npy"), "wb") as f:
+        np.save(f, rel_representations[0](indices=None).detach().numpy())
+
+    more_than_one_representation_message = (
+        lambda ent_or_relation: f"There is more than one {ent_or_relation} representation for the trained model (see https://pykeen.readthedocs.io/en/stable/tutorial/first_steps.html?highlight=embedding#using-learned-embeddings). You may want to inspect the others by loading the PyTorch model using the model pickle."
+    )
+
+    if len(ent_representations) > 1:
+        print(more_than_one_representation_message("entity"))
+    if len(rel_representations) > 1:
+        print(more_than_one_representation_message("relation"))
+
+    # Save entity-to-ID and relation-to-ID mappings. IDs correspond to rows of the embeddings matrices.
+    with open(os.path.join(output_path, "entities_to_ids.json"), "w") as f:
+        json.dump(tf.entity_to_id, f)
+
+    with open(os.path.join(output_path, "relations_to_ids.json"), "w") as f:
+        json.dump(tf.relation_to_id, f)
+
+    # Save plots
     if plot_losses:
         results.plot_losses()
         plt.savefig(os.path.join(output_path, "losses.png"), dpi=300)
