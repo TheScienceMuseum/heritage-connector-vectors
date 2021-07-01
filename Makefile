dba79d196551e35db2defbc55fe630ec92447004
Kalyan Dutia <kalyan.dutia@gmail.com>
2021-07-01 11:08:55 +0100

    filter triples using a list of predicates

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
-.PHONY: init 
+.PHONY: init clean
 
 init:
 	pip install -r requirements_min.txt && pip install -r requirements_dev.txt
 	pip freeze > requirements.txt
 	pre-commit install && pre-commit autoupdate
+
+clean:
+	rm -f ./data/interim/*
+
+./data/interim/triples_filtered_by_predicate.csv: ./data/raw/hc_dump_latest.csv ./config/predicate_filter.csv
+	python src/cli/filter_data_by_predicate.py -i ./data/raw/hc_dump_latest.csv -o ./data/interim/triples_filtered_by_predicate.csv -p ./config/predicate_filter.csv
---
+++ src/cli/filter_data_by_predicate.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import click
+from log import get_logger
+
+logger = get_logger(__name__)
+
+
+@click.command()
+@click.option("-i", "--input_path", required=True)
+@click.option("-o", "--output_path", required=True)
+@click.option("-p", "--predicate_filter_path", required=True)
+def filter_triples_tsv_by_predicates(
+    input_path: str, output_path: str, predicate_filter_path: str
+):
+    triples = pd.read_csv(
+        input_path, sep="\t", names=["subject", "predicate", "object"]
+    )
+    predicate_filter = pd.read_csv(predicate_filter_path)
+    predicates_keep = predicate_filter.loc[
+        predicate_filter["keep"] == 1, "predicate"
+    ].tolist()
+
+    filtered_triples = triples[triples["predicate"].isin(predicates_keep)]
+    filtered_triples.to_csv(output_path, sep="\t", index=False, header=False)
+    logger.debug(
+        f"Input no triples: {len(triples):,}. No predicates kept: {len(predicates_keep):,}/{len(predicate_filter):,}. Output no triples: {len(filtered_triples):,}."
+    )
+
+
+if __name__ == "__main__":
+    filter_triples_tsv_by_predicates()
