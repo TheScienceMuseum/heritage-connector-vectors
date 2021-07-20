86b9b7086389c9fa8ef0179a5ba01a96f2e01664
Kalyan Dutia <kalyan.dutia@gmail.com>
2021-07-20 16:45:28 +0100

    rename api to /neighbours; supports multiple entities at once

--- src/api.py
+++ src/api.py
@@ -1,6 +1,8 @@
 import argparse
 import os
-from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 import uvicorn
 from src.embedding_store import KGEmbeddingStore
 from src.nearest_neighbours import FaissNearestNeighbours
@@ -22,14 +24,29 @@ faiss_index = FaissNearestNeighbours(embedding_store).fit("entities")
 app = FastAPI()
 
 
-@app.get("/nearest_neighbours")
-@app.post("/nearest_neighbours")
-async def get_nearest_neighbours(entity: str, k: int):
-    entities, distances = faiss_index.search([entity], k)
-    entities = entities[0]
-    distances = distances[0].tolist()
+class NeighboursRequest(BaseModel):
+    entities: List[str]
+    k: int
 
-    return [(entities[idx], distances[idx]) for idx in range(len(entities))]
+
+@app.post("/neighbours")
+async def get_nearest_neighbours(request: NeighboursRequest):
+    neighbours, distances = faiss_index.search(request.entities, request.k)
+
+    response = []
+
+    for idx, ent in enumerate(request.entities):
+        if ent != neighbours[idx][0]:
+            raise HTTPException(
+                status_code=404,
+                detail=f"It looks like there's a mismatch between a request entity and its nearest neighbour. Problem entity: {ent}",
+            )
+
+        response.append(
+            {ent: list(zip(neighbours[idx][1:], distances[idx][1:].tolist()))}
+        )
+
+    return response
 
 
 if __name__ == "__main__":
